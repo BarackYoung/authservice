74610f9ab9375a0440685a79a5b18b3815362d48
复旦杨连欢 <1330292703@qq.com>
2025-08-15 23:03:34 +0800

    新增序列化版本

--- api/src/main/java/com/atom/authservice/api/login/bean/LoginResultResp.java
+++ api/src/main/java/com/atom/authservice/api/login/bean/LoginResultResp.java
@@ -2,6 +2,7 @@ package com.atom.authservice.api.login.bean;
 
 import lombok.Data;
 
+import java.io.Serial;
 import java.io.Serializable;
 
 /**
@@ -12,6 +13,9 @@ import java.io.Serializable;
  */
 @Data
 public class LoginResultResp implements Serializable {
+    @Serial
+    private static final long serialVersionUID = 1L;
+
     private Object authToken;
 
     private Object refreshToken;
